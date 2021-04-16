3b4996896409c1ea83bc130d40cd9b9844863752
krisnadp <krisna10permana@gmail.com>
2021-04-17 01:38:52 +0700

    fixing login #2

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,27 +1,73 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+@section('main-content')
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">{{ __('About') }}</h1>
 
-    <title>About</title>
-  </head>
-  <body>
-    <h1>Ini halaman about</h1>
+    <div class="row justify-content-center">
 
-    <!-- Optional JavaScript; choose one of the two! -->
+        <div class="col-lg-8">
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+            <div class="card shadow mb-4">
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-    -->
-  </body>
-</html>
+                <div class="card-profile-image mt-4">
+                    <img src="{{ asset('img/favicon.png') }}" class="rounded-circle" alt="user-image">
+                </div>
+
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="col-lg-12 mb-1">
+                            <div class="text-center">
+                                <h5 class="font-weight-bold">Alejandro RH</h5>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-4 mb-1 text-center">
+                            <a href="https://facebook.com/aleckrh" target="_blank" class="btn btn-facebook btn-circle btn-lg"><i class="fab fa-facebook-f fa-fw"></i></a>
+                        </div>
+                        <div class="col-md-4 mb-1 text-center">
+                            <a href="https://github.com/aleckrh" target="_blank" class="btn btn-github btn-circle btn-lg"><i class="fab fa-github fa-fw"></i></a>
+                        </div>
+                        <div class="col-md-4 mb-1 text-center">
+                            <a href="https://twitter.com/aleckrh" target="_blank" class="btn btn-twitter btn-circle btn-lg"><i class="fab fa-twitter fa-fw"></i></a>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h5 class="font-weight-bold">Laravel SB Admin 2</h5>
+                            <p>SB Admin 2 for Laravel.</p>
+                            <p>Recommend to install this preset on a project that you are starting from scratch, otherwise your project's design might break.</p>
+                            <a href="https://github.com/aleckrh/laravel-sb-admin-2" target="_blank" class="btn btn-github">
+                                <i class="fab fa-github fa-fw"></i> Go to repository
+                            </a>
+                        </div>
+                    </div>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h5 class="font-weight-bold">Credits</h5>
+                            <p>Laravel SB Admin 2 uses some open-source third-party libraries/packages, many thanks to the web community.</p>
+                            <ul>
+                                <li><a href="https://laravel.com" target="_blank">Laravel</a> - Open source framework.</li>
+                                <li><a href="https://github.com/DevMarketer/LaravelEasyNav" target="_blank">LaravelEasyNav</a> - Making managing navigation in Laravel easy.</li>
+                                <li><a href="https://startbootstrap.com/themes/sb-admin-2" target="_blank">SB Admin 2</a> - Thanks to Start Bootstrap.</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+@endsection
