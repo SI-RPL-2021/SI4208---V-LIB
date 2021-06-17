c317dd4807e9b487261729f02efbcd36ac3095e1
uliartha <arthaully80@gmail.com>
2021-06-17 20:51:13 +0700

    input konten

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,25 +9,24 @@
         <div class="col-md-6" style="">
         <h3>What is V-Lib?</h3>
             <p style="text-align: justify;">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Ullam nostrum modi at officia dolor deserunt consequuntur 
-                    vel repellendus quasi, maxime cumque esse qui in 
-                    laboriosam blanditiis, nesciunt doloribus! Doloremque, molestiae?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Ullam nostrum modi at officia dolor deserunt consequuntur 
-                    vel repellendus quasi, maxime cumque esse qui in 
-                    laboriosam blanditiis, nesciunt doloribus! Doloremque, molestiae?
+                    The concept of a library that provides a way for users
+                    to access information electronically.
+                    Virtual Library provide the concept that libraries can
+                    be presented to users without the user coming to the
+                    library directly. Users simply by accessing the internet
+                    can read digital collections owned by the library,
+                    it can even be termed "the library has our computer even
+                    on a cellphone that can be taken anywhere without space
+                    and time restrictions".
             </p>
             <h3>What can I search on V-Lib?</h3>
             <p style="text-align: justify;">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Ullam nostrum modi at officia dolor deserunt consequuntur 
-                    vel repellendus quasi, maxime cumque esse qui in 
-                    laboriosam blanditiis, nesciunt doloribus! Doloremque, molestiae?
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                    Ullam nostrum modi at officia dolor deserunt consequuntur 
-                    vel repellendus quasi, maxime cumque esse qui in 
-                    laboriosam blanditiis, nesciunt doloribus! Doloremque, molestiae?
+                    To meet the needs of our users, we subscribe to
+                    various online digital library materials (e-Resources)
+                    such as journals, ebooks, and other online reference works.
+                    Every member of the Virtual Library and already has a member number
+                    legitimate, entitled to take advantage of digital collection services
+                    online that we subscribe to (e-Resources).
             </p>
         </div>
         <div class="col-md-6">
